906657944659e287ce5a2d55ef8f45d0ed035102
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-24 16:54:03 -0300

    ...

--- test/WriteFileTest.cs
+++ test/WriteFileTest.cs
@@ -2,7 +2,14 @@ using System;
 using System.IO;
 
 public class WriteFileTest{
+	void test(){
+		File.WriteAllText("test.txt", "helloworld");
+	}
+
 	public static void Main(){
-		File.WriteAllText("test.txt", "hellworld");
+		StreamWriter w = new StreamWriter("test.txt",true);
+		w.WriteLine("hello");
+		w.WriteLine("world");
+		w.Close();	
 	}
 }
